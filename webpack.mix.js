afdaf1a7858bfd45047bbc7aaf1dcdf251835e63
noogen <tom@noogen.net>
2021-07-16 19:14:06 -0500

    update build

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,8 +13,8 @@ const webpackConfig = {
       jQuery: 'jquery'
     }),
     new webpack.DefinePlugin({
-      __VUE_OPTIONS_API__: 'true',
-      __VUE_PROD_DEVTOOLS__: 'false'
+      __VUE_OPTIONS_API__: true,
+      __VUE_PROD_DEVTOOLS__: false
     })
   ],
   resolve: {
